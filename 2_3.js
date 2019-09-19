7d000bd76c0b1d85c058cf87b410689f53e9cd6d
Raul <raul.muser@gmail.com>
2019-09-19 21:58:28 +0300

    Fix type coercion warning

--- 2_3.js
+++ 2_3.js
@@ -4,7 +4,7 @@ let board = "";
 for (let i = 0; i < size; i++) {
     let line = "";
     for (let j = 0; j < size; j++) {
-        nextChar = (j + i) % 2 == 0 ? "#" : " ";
+        nextChar = (j + i) % 2 === 0 ? "#" : " ";
         line += nextChar;
     }
     board += line + "\n"
