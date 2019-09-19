6d63316de48d9b2cf85e5f289813e065169e74ab
Raul <raul.muser@gmail.com>
2019-09-19 21:53:35 +0300

    Update the code to use the newline character
    Assignment specified to use the newline character to separate lines

--- 2_3.js
+++ 2_3.js
@@ -1,11 +1,12 @@
 // Chessboard
 let size = 8;
-
+let board = "";
 for(let i = 0; i < size; i++){
-  	let output = "";
+  	let line = "";
 	for(let j = 0; j < size; j++){
         nextChar = (j+i)%2==0 ? "#" : " ";
-        output+=nextChar;
+        line+=nextChar;
 	}
-  	console.log(output);
+  	board+=line+"\n"
 }
+console.log(board);
