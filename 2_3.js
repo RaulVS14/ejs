12731698712fa5502a7dbaa538d38503356ec829
Raul <raul.muser@gmail.com>
2019-09-19 21:56:14 +0300

    Fix code indentation

--- 2_3.js
+++ 2_3.js
@@ -1,12 +1,12 @@
 // Chessboard
 let size = 8;
 let board = "";
-for(let i = 0; i < size; i++){
-  	let line = "";
-	for(let j = 0; j < size; j++){
-        nextChar = (j+i)%2==0 ? "#" : " ";
-        line+=nextChar;
-	}
-  	board+=line+"\n"
+for (let i = 0; i < size; i++) {
+    let line = "";
+    for (let j = 0; j < size; j++) {
+        nextChar = (j + i) % 2 == 0 ? "#" : " ";
+        line += nextChar;
+    }
+    board += line + "\n"
 }
 console.log(board);
