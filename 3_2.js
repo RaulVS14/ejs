fe007802b621065bba58c2398d7812d64536dc44
Raul <raul.muser@gmail.com>
2019-09-24 19:29:58 +0300

    Remove dependency of outer library

--- 3_2.js
+++ 3_2.js
@@ -1,10 +1,11 @@
 // Recursion
 function isEven(number) {
-    number = Math.abs(number);
     if (number === 0) {
         return true;
     } else if (number === 1) {
         return false;
+    } else if (number < 0) {
+        return isEven(-number);
     } else {
         return isEven(number - 2);
     }
